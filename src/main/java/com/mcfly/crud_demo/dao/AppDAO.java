ee7f6128ce72176c3037c8c1224e3a7a367088b6
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-20 22:28:52 +0300

    + AppDAO

---
+++ src/main/java/com/mcfly/crud_demo/dao/AppDAO.java
@@ -0,0 +1,8 @@
+package com.mcfly.crud_demo.dao;
+
+import com.mcfly.crud_demo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor instructor);
+}
---
+++ src/main/java/com/mcfly/crud_demo/dao/AppDAOImpl.java
@@ -0,0 +1,24 @@
+package com.mcfly.crud_demo.dao;
+
+import com.mcfly.crud_demo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    @Override
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+}
