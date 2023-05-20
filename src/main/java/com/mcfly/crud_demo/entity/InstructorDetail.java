0db3104d6b0d33767840d1b688e4dc0f85840216
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-20 22:17:54 +0300

    + Instructor & InstructorDetail entities

---
+++ src/main/java/com/mcfly/crud_demo/entity/InstructorDetail.java
@@ -0,0 +1,62 @@
+package com.mcfly.crud_demo.entity;
+
+import jakarta.persistence.*;
+
+import java.util.StringJoiner;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    public InstructorDetail() {
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", InstructorDetail.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("youtubeChannel='" + youtubeChannel + "'")
+                .add("hobby='" + hobby + "'")
+                .toString();
+    }
+}
